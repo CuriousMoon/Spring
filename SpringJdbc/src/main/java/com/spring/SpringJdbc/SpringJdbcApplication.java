c257e6eb7c3653ba4cb6fa1f09a07d7a1d1e7462
gupta.mansi <imansigupta114@gmail.com>
2021-11-02 21:19:14 +0530

    Adding user menu for selecting option

--- SpringJdbc/src/main/java/com/spring/SpringJdbc/SpringJdbcApplication.java
+++ SpringJdbc/src/main/java/com/spring/SpringJdbc/SpringJdbcApplication.java
@@ -1,5 +1,8 @@
 package com.spring.SpringJdbc;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -11,18 +14,76 @@ import com.spring.SpringJdbc.entities.Student;
 @SpringBootApplication
 public class SpringJdbcApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		Student student;
+		int id;
+		String name, city,choice;
 		System.out.println("My Application Started...");
 		ApplicationContext context = new ClassPathXmlApplicationContext("com/spring/SpringJdbc/Config.xml");
-		StudentDao studentDao = context.getBean("studentDaoImpl",StudentDao.class);
-		Student student = new Student(222,"Ankit","Chadigarh");
-		System.out.println("Number of records inserted "+studentDao.insert(student));
-		student.setId(222);
-		student.setName("Ankit Vashisth");
-		student.setCity("Faridabad");
-		System.out.println("Number of records updated "+studentDao.updateValues(student));
-		System.out.println("Number of records deleted "+studentDao.delete(222));
-		//SpringApplication.run(SpringJdbcApplication.class, args);
+		StudentDao studentDao = context.getBean("studentDaoImpl", StudentDao.class);
+		System.out.println("Choose from the below options");
+		SpringJdbcApplication.displayChoice();
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		
+		boolean flag = true;
+
+		while (flag) {
+			choice = reader.readLine();
+			switch (choice) {
+			case "1":
+				System.out.println("\ninsert id of the student: \n");
+				id = Integer.parseInt(reader.readLine());
+				System.out.println("\ninsert name of the student: \n");
+				name = reader.readLine();
+				System.out.println("\n");
+				System.out.println("insert city of the student: \n");
+				city = reader.readLine();
+				student = new Student(id, name, city);
+				System.out.println("Number of records inserted " + studentDao.insert(student));
+				SpringJdbcApplication.displayChoice();
+				break;
+			case "2":
+				System.out.println("\ninsert id of the student to be updated: \n");
+				id = Integer.parseInt(reader.readLine());
+				System.out.println("\ninsert name of the student to be updated:: \n");
+				name = reader.readLine();
+				System.out.println("\ninsert city of the student to be updated:: \n");
+				city = reader.readLine();
+				student = new Student(id, name, city);
+				System.out.println("Number of records updated " + studentDao.updateValues(student));
+				SpringJdbcApplication.displayChoice();
+				break;
+			case "3":
+				System.out.println("\ninsert id of the student to be deleted: \n");
+				id = Integer.parseInt(reader.readLine());
+				System.out.println("Number of records updated " + studentDao.delete(id));
+				SpringJdbcApplication.displayChoice();
+				break;
+			case "4":
+				SpringJdbcApplication.displayChoice();
+				break;
+			case "0":
+				flag = false;
+				System.out.println("Bye!");
+				break;
+			default:
+				System.out.println("Incorrect choice");
+				System.out.println("Please select from given menu\n");
+				SpringJdbcApplication.displayChoice();
+				break;
+			}
+		}
 	}
+
+	public static void displayChoice() {
+		System.out.println("1- For Insert Operation");
+		System.out.println("2- For Update Operation");
+		System.out.println("3- For Delete Operation");
+		System.out.println("4- For Displaying Menu");
+		System.out.println("0- For Exit");
+		System.out.println("==========================================");
+		System.out.println("Please enter your choice in next line \n");
+	}
+
 
 }
--- SpringJdbc/src/main/java/com/spring/SpringJdbc/dao/StudentDaoImpl.java
+++ SpringJdbc/src/main/java/com/spring/SpringJdbc/dao/StudentDaoImpl.java
@@ -4,26 +4,42 @@ import org.springframework.jdbc.core.JdbcTemplate;
 
 import com.spring.SpringJdbc.entities.Student;
 
-public class StudentDaoImpl implements StudentDao{
+public class StudentDaoImpl implements StudentDao {
 
-	private JdbcTemplate  jdbcTemplate; 
-	
+	private JdbcTemplate jdbcTemplate;
+
 	@Override
 	public int insert(Student student) {
 		String query = "insert into student(id,name,city) values(?,?,?)";
-		return jdbcTemplate.update(query, student.getId(),student.getName(),student.getCity());
+		try {
+			return jdbcTemplate.update(query, student.getId(), student.getName(), student.getCity());
+		} catch (Exception e) {
+			System.out.println("Invalid Operation/Id already exists");
+			return -1;
+		}
 	}
-	
+
 	@Override
 	public int updateValues(Student student) {
 		String query = "update student set city=?, name=? where id=?";
-		return jdbcTemplate.update(query,student.getCity(),student.getName(),student.getId());
+		try {
+			return jdbcTemplate.update(query, student.getCity(), student.getName(), student.getId());
+		} catch (Exception e) {
+			System.out.println("Invalid Operation/Id does not exists");
+			return -1;
+		}
 	}
-	
+
 	@Override
 	public int delete(int studentId) {
 		String query = "delete from student where id=?";
-		return jdbcTemplate.update(query,studentId);
+		try {
+			return jdbcTemplate.update(query, studentId);
+		} catch (Exception e) {
+			System.out.println("Invalid Operation/Id does not exists");
+			return -1;
+		}
+
 	}
 
 	public JdbcTemplate getJdbcTemplate() {
@@ -33,11 +49,5 @@ public class StudentDaoImpl implements StudentDao{
 	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
 		this.jdbcTemplate = jdbcTemplate;
 	}
-
-	
-
-
-
-	
 
 }
